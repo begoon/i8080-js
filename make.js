d9fb16b7bac1af61ae794837ecef933a0d05f2c2
Alexander Demin <alexander.demin@onalytica.com>
2019-04-24 15:19:28 +0100

    Switch travis ci to use make.js

--- make.js
+++ make.js
@@ -8,22 +8,31 @@ let args = process.argv.slice(2);
 let cmd = args.length ? args[0] : "all";
 console.log(cmd, args);
 
-if (cmd == "all") {
-  node('rkdump >files.js');
-  build();
+if (cmd == "all") all();
+
+if (cmd == 'files') files();
+
+if (cmd == 'build') build();
+
+if (cmd == 'test') test();
+
+if (cmd == 'ex1') all(true);
+
+function test() { 
+  node("all.js"); 
 }
 
-if (cmd == 'clean-git') {
-  run('git clean -dfx');
+function all(ex1) {
+  files();
+  build(ex1);
+  timeit(test);
 }
 
-if (cmd == 'build') {
-  build();
-}
-
-function build() {
-  const files = ['files.js', 'i8080.js', 'i8080_disasm.js', 'i8080_trace.js', 'i8080_test.js', 'main.js'];
+function build(ex1) {
+  const files = ['files.js', 'i8080.js', 'i8080_disasm.js', 'i8080_trace.js', 'i8080_test.js'];
+  files.push(ex1 ? "main_ex1.js" : "main.js");
   let contents = [];
+  console.log(files);
   files.forEach((name) => {
     contents.push(fs.readFileSync(name));
     console.log(name, contents[contents.length - 1].length);
@@ -33,19 +42,27 @@ function build() {
   fs.writeFileSync("all.js", raw);
 }
 
+function files() {
+  node('rkdump >files.js');
+}
+
+if (cmd == 'clean') {
+  run('git clean -dfx');
+}
+
+// -------------------------------------
+
+function timeit(cb) {
+  const { performance } = require('perf_hooks');
+  const started = performance.now();
+  cb();
+  const duration = (performance.now() - started) / 1000;
+  console.log(`${duration.toFixed(2)}(s) ${(duration / 60).toFixed(2)}(m)`)
+}
+
 function run(cmd) {
-  console.log(`${cmd}`);
-  execSync(`${cmd}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    stdout ? console.log(stdout) : void 0;
-    if (stderr) {
-      console.log(`stderr:\n${stderr}`);
-      process.exit(2);
-    }
-  });
+  console.log(cmd);
+  console.log(execSync(cmd).toString());
 }
 
 function node(cmd) {
